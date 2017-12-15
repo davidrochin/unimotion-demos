f1e5091adc50acfdebb9bab3d98ed9f7698e9b5b
David Rochin <jdrc8@hotmail.com>
2017-12-15 15:41:56 -0700

    Nuevo Modelo 3D
    -Se empezó el proceso de reemplazar el modelo 3d dummy que se usaba para probar el Character Controller por un modelo original.
    -Se cambió la lógica que se usaba para determinar a que direccion debe caminar el Character al haber input.

--- 3D Character Controller/Assets/Scripts/CharacterControl.cs
+++ 3D Character Controller/Assets/Scripts/CharacterControl.cs
@@ -18,10 +18,10 @@ public class CharacterControl : MonoBehaviour {
         if(character.state == Character.State.OnGround || character.state == Character.State.OnAir) {
             if (GetInputVector() != Vector3.zero) {
                 character.RotateTowards(GetInputVector(), 400f * Time.deltaTime);
-                float speed = Vector3.ClampMagnitude(GetInputVector(), 1f).magnitude;
-                if (Input.GetButton("Walk")) { speed = Mathf.Clamp(speed, 0f, 0.5f); } 
-                else { speed = Mathf.Clamp(speed, 0.4f, 1f); }
-                character.Move(transform.forward, speed);
+                //float speed = Vector3.ClampMagnitude(GetInputVector(), 1f).magnitude;
+                //if (Input.GetButton("Walk")) { speed = Mathf.Clamp(speed, 0f, 0.5f); } 
+                //else { speed = Mathf.Clamp(speed, 0.4f, 1f); }
+                character.Move(transform.forward, GetInputMagnitude());
             }
         }
 
@@ -58,11 +58,28 @@ public class CharacterControl : MonoBehaviour {
         }
 
         //Transformar la direccion para que sea relativa a la camara.
-        Vector3 transDirection = Camera.main.transform.TransformDirection(input);
-
+        //Vector3 transDirection = Camera.main.transform.TransformDirection(input);
+        Quaternion tempQ = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+        Debug.Log(tempQ);
+        Vector3 transDirection = tempQ * input;
+        
         //Hacer que el Vector no apunte hacia arriba.
-        transDirection = new Vector3(transDirection.x, 0f, transDirection.z);
+        //transDirection = new Vector3(transDirection.x, 0f, transDirection.z).normalized;
+        finalMovementVector = transDirection;
         return transDirection;
+    }
+
+    float GetInputMagnitude() {
+        Vector3 input = Vector3.zero;
+        if (inputType == InputType.Normal) { input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")); } 
+        else if (inputType == InputType.Raw) { input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); }
+        return Vector3.ClampMagnitude(input, 1f).magnitude;
+    }
+
+    Vector3 finalMovementVector;
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, finalMovementVector);
     }
 
     public enum InputType { Normal, Raw }
