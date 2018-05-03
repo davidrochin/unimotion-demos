f9030527feafef32b1f20624d5ee9c80b0402739
David Rochin <jdrc8@hotmail.com>
2018-05-03 08:21:44 -0600

    Moved Unity project to root of repository

---
+++ Assets/3D Third Person/Scripts/CharacterControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+    public InputType inputType;
+
+    Character character;
+
+	void Awake () {
+        character = GetComponent<Character>();
+	}
+	
+	void Update () {
+
+        //Caminar y correr
+        if(character.state == Character.State.OnGround || character.state == Character.State.OnAir) {
+            if (GetInputVector() != Vector3.zero) {
+                character.RotateTowards(GetInputVector(), 400f * Time.deltaTime);
+                //float speed = Vector3.ClampMagnitude(GetInputVector(), 1f).magnitude;
+                //if (Input.GetButton("Walk")) { speed = Mathf.Clamp(speed, 0f, 0.5f); } 
+                //else { speed = Mathf.Clamp(speed, 0.4f, 1f); }
+                character.Move(transform.forward, GetInputMagnitude());
+            }
+        }
+
+        //Movimiento sobre ladera
+        if (character.state == Character.State.OnLedge) {
+            if (Input.GetAxisRaw("Horizontal") > 0f) {
+                character.LedgeMove(Vector3.right);
+            } else if (Input.GetAxisRaw("Horizontal") < 0f) {
+                character.LedgeMove(Vector3.left);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            if(character.state == Character.State.OnLedge) {
+                character.Climb();
+            } else {
+                character.Jump();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            character.Roll();
+        }
+	}
+
+    Vector3 GetInputVector() {
+        Vector3 input = Vector3.zero;
+
+        if(inputType == InputType.Normal) {
+            input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        } else if (inputType == InputType.Raw) {
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        //Transformar la direccion para que sea relativa a la camara.
+        //Vector3 transDirection = Camera.main.transform.TransformDirection(input);
+        Quaternion tempQ = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+        //Debug.Log(tempQ);
+        Vector3 transDirection = tempQ * input;
+        
+        //Hacer que el Vector no apunte hacia arriba.
+        //transDirection = new Vector3(transDirection.x, 0f, transDirection.z).normalized;
+        finalMovementVector = transDirection;
+        return transDirection;
+    }
+
+    float GetInputMagnitude() {
+        Vector3 input = Vector3.zero;
+        if (inputType == InputType.Normal) { input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")); } 
+        else if (inputType == InputType.Raw) { input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); }
+        return Vector3.ClampMagnitude(input, 1f).magnitude;
+    }
+
+    Vector3 finalMovementVector;
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, finalMovementVector);
+    }
+
+    public enum InputType { Normal, Raw }
+}
---
+++ Assets/Util/Util.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util {
+    public class Input {
+
+        public static Vector3 GetMouseMovement() {
+            //Obtener un vector a partir del movimiento del mouse y del Joystick
+            Vector3 input = new Vector3(UnityEngine.Input.GetAxis("Mouse X"), -UnityEngine.Input.GetAxis("Mouse Y"), 0f);
+            input = input + new Vector3(UnityEngine.Input.GetAxis("Camera Horizontal") * 2f, UnityEngine.Input.GetAxis("Camera Vertical") * 2f, 0f);
+            return input;
+        }
+
+    }
+}
